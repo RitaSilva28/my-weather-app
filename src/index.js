807ee18cf3c6fccef0ec72e7848ce83299990d35
Rita Silva <rita.cfs28@gmail.com>
2021-12-07 14:15:01 +0000

    changed apikey from forecast

--- src/index.js
+++ src/index.js
@@ -219,7 +219,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
 
-    let apiKey = "dc8dede1ef33bea8aaa397f04b2d3b55";
+    let apiKey = "8cd9be374c7c96c39a9fe73f4bf2f055";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
 
     axios.get(apiUrl).then(displayForecast);
